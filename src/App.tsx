a5c3c2764c9495d8f863fa34cc97028f0acf09dc
phernandez <phernandez@basicmachines.co>
2024-10-06 15:00:23 -0500

    popover v1

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import {Popover, PopoverContent, PopoverTrigger,} from "@/components/ui/popover"
 
 // import Link from "next/link"
 
@@ -70,9 +71,66 @@ function App() {
             <SelectScrollable/>
             <TableDemo/>
             <DialogDemo/>
+            <PopoverDemo/>
         </ThemeProvider>
     )
 }
+
+
+export function PopoverDemo() {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant="outline">Open popover</Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+                <div className="grid gap-4">
+                    <div className="space-y-2">
+                        <h4 className="font-medium leading-none">Dimensions</h4>
+                        <p className="text-sm text-muted-foreground">
+                            Set the dimensions for the layer.
+                        </p>
+                    </div>
+                    <div className="grid gap-2">
+                        <div className="grid grid-cols-3 items-center gap-4">
+                            <Label htmlFor="width">Width</Label>
+                            <Input
+                                id="width"
+                                defaultValue="100%"
+                                className="col-span-2 h-8"
+                            />
+                        </div>
+                        <div className="grid grid-cols-3 items-center gap-4">
+                            <Label htmlFor="maxWidth">Max. width</Label>
+                            <Input
+                                id="maxWidth"
+                                defaultValue="300px"
+                                className="col-span-2 h-8"
+                            />
+                        </div>
+                        <div className="grid grid-cols-3 items-center gap-4">
+                            <Label htmlFor="height">Height</Label>
+                            <Input
+                                id="height"
+                                defaultValue="25px"
+                                className="col-span-2 h-8"
+                            />
+                        </div>
+                        <div className="grid grid-cols-3 items-center gap-4">
+                            <Label htmlFor="maxHeight">Max. height</Label>
+                            <Input
+                                id="maxHeight"
+                                defaultValue="none"
+                                className="col-span-2 h-8"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </PopoverContent>
+        </Popover>
+    )
+}
+
 
 export function DialogDemo() {
     return (
